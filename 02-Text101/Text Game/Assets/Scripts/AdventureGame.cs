6d58b80a66bf8c2dc08bfe0f38f7dbc95e24c178
Roberto Higor <roberto.higor@outlook.com>
2020-05-18 18:48:59 -0300

    Adicionado estados e gerenciamento de estados. Adicionado alguns textos.

---
+++ 02-Text101/Text Game/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Exibe essa variável no Unity inspector.
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState; // Estado inicial
+
+    State state; // Estado atual
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        // Altera o componente Text do GameObject
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    public void ManageState()
+    {
+        // Var atribui o tipo assim que um valor é definido.
+        var nextState = state.GetNextState();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) // Else pois não pode ser permitido apertar 2 botões ao mesmo tempo.
+        {
+            state = nextState[1];
+        }
+
+        textComponent.text = state.GetStateStory(); // Atualizar o texto
+    }
+}
---
+++ 02-Text101/Text Game/Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adiciona o State no menu de criação de Assets
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // (minSize, linhas)
+    [TextArea(14,10)] [SerializeField]
+    string storyText;
+    [SerializeField]
+    State[] nextStates; 
+
+    public string GetStateStory() 
+    {
+        return storyText;
+    }
+
+    public State[] GetNextState(){
+        return nextStates;
+    }
+}
