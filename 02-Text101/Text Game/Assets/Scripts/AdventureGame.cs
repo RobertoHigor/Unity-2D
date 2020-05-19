dc85cf0cdf21c5027c495f3107b0ba5a9320b2f6
Roberto Higor <roberto.higor@outlook.com>
2020-05-19 16:53:28 -0300

    Adicionado For loop para os comandos. Finalizado o projeto

--- 02-Text101/Text Game/Assets/Scripts/AdventureGame.cs
+++ 02-Text101/Text Game/Assets/Scripts/AdventureGame.cs
@@ -3,39 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AdventureGame : MonoBehaviour
-{
+public class AdventureGame : MonoBehaviour {
     // Exibe essa variável no Unity inspector.
     [SerializeField] Text textComponent;
     [SerializeField] State startingState; // Estado inicial
 
     State state; // Estado atual
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
         state = startingState;
         // Altera o componente Text do GameObject
-        textComponent.text = state.GetStateStory();
+        textComponent.text = state.GetStateStory ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        ManageState();
+    void Update () {
+        ManageState ();
     }
 
-    public void ManageState()
-    {
+    public void ManageState () {
         // Var atribui o tipo assim que um valor é definido.
-        var nextState = state.GetNextState();
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            state = nextState[0];
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) // Else pois não pode ser permitido apertar 2 botões ao mesmo tempo.
-        {
-            state = nextState[1];
+        var nextState = state.GetNextState ();
+
+        for (int i = 0; i < nextState.Length; i++) {
+            if (Input.GetKeyDown (KeyCode.Alpha1 + i)) { // Como o índice começa em 0, a primeira tecla será 1
+                state = nextState[0];
+            } 
         }
 
-        textComponent.text = state.GetStateStory(); // Atualizar o texto
+        textComponent.text = state.GetStateStory (); // Atualizar o texto
     }
 }
