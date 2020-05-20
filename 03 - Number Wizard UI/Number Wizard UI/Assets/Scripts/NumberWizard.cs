93e2bf14a43e3bbb021add225643962dd848e488
Roberto Higor <roberto.higor@outlook.com>
2020-05-20 15:59:07 -0300

    Criado Number Wizard com Interface de botões

---
+++ 03 - Number Wizard UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    // Variável do tipo TextMesh Pro para exibir o Guess
+    // Nesse caso, é associado um objeto do tipo TextMesh
+    [SerializeField] TextMeshProUGUI guessText; 
+    int guess;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+	}
+	
+    void StartGame()
+    {        
+        guess = (max + min ) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1; // Para arredondar, pois max - min / 2 daria 999 / 2
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
---
+++ 03 - Number Wizard UI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Para usar o namespace do SceneManagement
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // Atribuir o buildIndex da cena atual
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
