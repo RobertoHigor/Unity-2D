320b6b8f5286a1a2c84308be78b1657280755baf
Roberto Higor <roberto.higor@outlook.com>
2020-05-21 19:25:07 -0300

    Calculando distância, prendendo a bola e utilizando Script Execution Order

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Ball - Paddle 1 = Distância entre a bola e o paddle
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 paddlePos = new Vector2(
+            paddle1.transform.position.x, 
+            paddle1.transform.position.y
+        );
+
+        transform.position = paddlePos + paddleToBallVector;
     }
 }
