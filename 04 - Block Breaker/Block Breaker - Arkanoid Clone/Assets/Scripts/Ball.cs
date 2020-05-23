0b540b99d5ba49e599fb82af934635bb71a26010
Roberto Higor <roberto.higor@outlook.com>
2020-05-23 17:52:20 -0300

    Feito script dos controles. Colisão para destruir blocos

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xForce = 2f;
+    [SerializeField] float yForce = 15f;
+    bool hasStarted = false;
 
     // state
     Vector2 paddleToBallVector;
@@ -18,11 +21,30 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(
-            paddle1.transform.position.x, 
+            paddle1.transform.position.x,
             paddle1.transform.position.y
         );
-
+        // A posição da bola segue a posição do paddle
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick()
+    {        
+        if (Input.GetMouseButtonUp(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xForce, yForce);
+        }
     }
 }
