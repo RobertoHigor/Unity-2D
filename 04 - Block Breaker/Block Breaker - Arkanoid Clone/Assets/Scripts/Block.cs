2a539bb270ce4a741f7ffcc187f7424d681cb1a5
Roberto Higor <roberto.higor@outlook.com>
2020-05-24 16:31:08 -0300

    Criado 2 fases. Criado Prefabs e utilizando blocos com SNap

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Block.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Block.cs
@@ -6,7 +6,8 @@ public class Block : MonoBehaviour
 {
    private void OnCollisionEnter2D(Collision2D collision) {
        // gameObject é uma referência a esse objeto
-       Destroy(gameObject, 1f);
+       //Destroy(gameObject, 1f);
+       Destroy(gameObject);
        Debug.Log(collision.gameObject.name);
    }
 }
