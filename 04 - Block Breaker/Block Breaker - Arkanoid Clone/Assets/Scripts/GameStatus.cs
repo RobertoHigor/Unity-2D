547d27bc0895ca7c8d86a19d6f2e7dbfd526b2b7
Roberto Higor <roberto.higor@outlook.com>
2020-05-27 00:06:03 -0300

    Utilizando Singleton para persistir o placar entre as cenas

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/GameStatus.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/GameStatus.cs
@@ -6,16 +6,34 @@ using UnityEngine.UI;
 public class GameStatus : MonoBehaviour
 {
     // Config params
-    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int scorePerBlockDestroyed = 83;
     [SerializeField] Text scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
 
+    private void Awake()
+    {
+        // Irá armazenar quantos objetos GameStatus existem
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        // Se tiver mais de um
+        if (gameStatusCount > 1) 
+        {
+            // Desativando o objeto antes de destruir
+            // Isso se deve pois o método Destroy somente é executado no final
+            // da execução do Unity.
+            gameObject.SetActive(false);
+            Destroy(gameObject); // Destruir o próprio objeto
+        } else 
+        {
+            // Não destruir quando o level carregar
+            DontDestroyOnLoad(gameObject); 
+        }
+    }
     private void Start()
     {
-        scoreText.text = "0";
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
