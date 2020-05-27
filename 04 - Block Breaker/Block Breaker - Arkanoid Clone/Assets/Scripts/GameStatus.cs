d42c269f70d02ba6375a4d3396273813c0f661b4
Roberto Higor <roberto.higor@outlook.com>
2020-05-26 23:40:32 -0300

    Interface do placar

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/GameStatus.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/GameStatus.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStatus : MonoBehaviour
 {
     // Config params
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int scorePerBlockDestroyed = 83;
+    [SerializeField] Text scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = "0";
+    }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+        scoreText.text = currentScore.ToString();
     }
 
     public void AddToScore()
