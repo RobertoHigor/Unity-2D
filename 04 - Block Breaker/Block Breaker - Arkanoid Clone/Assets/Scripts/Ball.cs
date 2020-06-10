b32b41d6b11dc0ee55a3771f406eb30df4532d83
Roberto Higor <roberto.higor@outlook.com>
2020-06-10 18:03:56 -0300

    Adicionado aleatoriedade na velocidade da bola

--- 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
+++ 04 - Block Breaker/Block Breaker - Arkanoid Clone/Assets/Scripts/Ball.cs
@@ -1,20 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    // config params
     [SerializeField] Paddle paddle1;
     [SerializeField] float xForce = 2f;
     [SerializeField] float yForce = 15f;
     [SerializeField] AudioClip[] ballSounds;
-  
+    [SerializeField] float randomFactor = 0.2f;
+
     // state
     bool hasStarted = false;
     Vector2 paddleToBallVector;
 
     // Cached component references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,7 @@ public class Ball : MonoBehaviour
         // Ball - Paddle 1 = Distância entre a bola e o paddle
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -32,7 +34,7 @@ public class Ball : MonoBehaviour
             LockBallToPaddle();
             LaunchOnMouseClick();
         }
-        
+
     }
 
     private void LockBallToPaddle()
@@ -46,23 +48,29 @@ public class Ball : MonoBehaviour
     }
 
     private void LaunchOnMouseClick()
-    {        
+    {
         if (Input.GetMouseButtonUp(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xForce, yForce);
+            myRigidBody2D.velocity = new Vector2(xForce, yForce);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) 
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(
+            Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor)
+        );
+
        if (hasStarted)
-       {
-        if (collision.gameObject.name != "Paddle") 
         {
-            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
-            myAudioSource.PlayOneShot(clip); 
+            if (collision.gameObject.name != "Paddle") // se o objeto de colisão não for o Paddle
+            {
+                AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+                myAudioSource.PlayOneShot(clip);
+            }
+            myRigidBody2D.velocity += velocityTweak;
         }
-       }
     }
 }
