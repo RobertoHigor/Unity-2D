0f7915a35ee186121de1e24eeaa6f48ee7d8a346
Roberto Higor <roberto.higor@outlook.com>
2020-07-02 16:45:52 -0300

    Utilizando ViewportToWorldPoint() para limitar a tela do jogo

--- 05 - Laser Defender/Laser Defender/Assets/Script/Player.cs
+++ 05 - Laser Defender/Laser Defender/Assets/Script/Player.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeFIeld] float padding = 1f; // padding entre as bordas
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -24,10 +30,18 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        var newXPos = transform.position.x + deltaX;  
-        var newYPos = transform.position.y + deltaY; 
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);  
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax); 
         transform.position = new Vector2(newXPos, newYPos);   
+    }
 
-
+    private void SetUpMoveBoundaries() 
+    {
+        Camera gameCamera = Camera.main;
+        // World space value do X
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding; 
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
