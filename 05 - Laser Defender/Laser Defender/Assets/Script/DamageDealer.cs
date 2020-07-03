6f23672a4054acbb6ced028f1eac571f80d86b7c
Roberto Higor <roberto.higor@outlook.com>
2020-07-03 16:24:04 -0300

    Criado sistema de dano de projéteis e vida dos inimigos

---
+++ 05 - Laser Defender/Laser Defender/Assets/Script/DamageDealer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    // Falta acionar o Hit para destruir o projétil
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ 05 - Laser Defender/Laser Defender/Assets/Script/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        // Acessando o componente damageDealer para pegar o dano
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
