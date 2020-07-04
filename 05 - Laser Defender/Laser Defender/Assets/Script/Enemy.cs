92091e9abb2fc2dd8ff88f2d66f5e37980987f0b
Roberto Higor <roberto.higor@outlook.com>
2020-07-04 15:28:22 -0300

    Funcionalidade do inimigo atirar em intervalos aleatórios

--- 05 - Laser Defender/Laser Defender/Assets/Script/Enemy.cs
+++ 05 - Laser Defender/Laser Defender/Assets/Script/Enemy.cs
@@ -5,24 +5,53 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 2;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyProjectile;
+    [SerializeField] float projectileSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      CountDownAndShoot();  
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime; // - o tempo que o frame demora.
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(
+            enemyProjectile,
+            transform.position, 
+            Quaternion.identity) as GameObject;
+
+        projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         // Acessando o componente damageDealer para pegar o dano
-        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        health -= damageDealer.GetDamage();
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();       
+        ProcessDamage(damageDealer);
+    }
 
+    private void ProcessDamage(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
         if (health <= 0)
         {
             Destroy(gameObject);
