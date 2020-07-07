3625efb54828814d2f566ad8009573c3f6b0f723
Roberto Higor <roberto.higor@outlook.com>
2020-07-07 16:47:13 -0300

    Splash screen que abre o menu depois de x segundos

---
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    private void Start() {
+        // Pegar o build index da cena atual
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            Debug.Log("Dentro da Splash Screen. Carregando próxima cena...");
+            StartCoroutine(DelayStartScren());
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator DelayStartScren()
+    {
+        yield return new WaitForSeconds(3);
+        LoadNextScene();
+    }
+}
