dadbf180771c1d3e04394df08f4c758c1d1827b9
Roberto Higor <roberto.higor@outlook.com>
2020-07-10 17:15:42 -0300

    Colocar cacto na posição do mouse

--- 06 - Glitch Garden/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -7,15 +7,25 @@ public class DefenderSpawner : MonoBehaviour
     [SerializeField] GameObject defender;
 
     private void OnMouseDown() {        
-        SpawnDefender();
+        SpawnDefender(GetSquarePosition());
         Debug.Log("mouse was clicked");
     }
 
-    private void SpawnDefender()
+    private Vector2 GetSquarePosition()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        // Converter a coordenada do world screen para o world space para obter a coordenada do grid
+        // Nesse caso, converter a posição do mouse para World Point
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+        return worldPos;
+    }
+
+    private void SpawnDefender(Vector2 worldPos)
     {
         GameObject newDefender = Instantiate(
             defender,
-            transform.position,
+            worldPos,
             Quaternion.identity
         );
     }
