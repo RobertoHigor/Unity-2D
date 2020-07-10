b0f87630c1de0a567d97533a6b072921cb87584c
Roberto Higor <roberto.higor@outlook.com>
2020-07-10 16:50:16 -0300

    Colocar um personagem ao clicar na tela. Delimitar onde se pode adicionar personagens.

---
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown() {        
+        SpawnDefender();
+        Debug.Log("mouse was clicked");
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(
+            defender,
+            transform.position,
+            Quaternion.identity
+        );
+    }
+}
