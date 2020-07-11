05e03d18d00e2d8d1eef4d95f74f628a5477c066
Roberto Higor <roberto.higor@outlook.com>
2020-07-11 00:46:23 -0300

    Criado a interface para selecionar o defender. Criado um foreach para escurecer todos os defenders. Após clicar em um, ele é clareado na interface.

---
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown() {
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        // loop para deixar todos os botões escuros novamente
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+        
+        // Alterar o botão selecionado para branco.
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
