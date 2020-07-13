6ac389123801ce9c284e381639778d9b3bc51dbc
Roberto Higor <roberto.higor@outlook.com>
2020-07-13 19:49:16 -0300

    Adicionado animação de ataque para o Lizard. Lizard agora para de se movimentar para atacar. Adicionado túmulo.

--- 06 - Glitch Garden/Glitch Garden/Assets/Scripts/Attacker.cs
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/Attacker.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range (0f, 5f)]
+    [Range(0f, 5f)]
     float currentSpeed = 1f;
+    GameObject currentTarget;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Mover automaticamente.
-        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);  
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
     }
 
-    public void SetMovementSpeed(float speed) {
-        {
-           currentSpeed = speed; 
-        }
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
+
+    // Método para ativar a animação de ataque e especificar o alvo
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
 }
