8bb050acc0912edb9ac4e03fea8e01d9da9febfc
Roberto Higor <roberto.higor@outlook.com>
2020-07-13 20:59:33 -0300

    Adicionado o inimigo Fox. Fox pula os túmulos.

---
+++ 06 - Glitch Garden/Glitch Garden/Assets/Scripts/Fox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        GameObject collisionObject = collision.gameObject;
+        
+        // Se for um tumulo, pular
+        if (collisionObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }      
+        else if (collisionObject.GetComponent<Defender>())
+        {  // Senão, se for defender: atacar
+            GetComponent<Attacker>().Attack(collisionObject);
+        }
+        
+    }
+}
